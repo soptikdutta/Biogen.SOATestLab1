2428d975abe372adb173d91db1886351f58b5589
CORP\sdutta6 <sdutta@deloitte.com>
2017-10-04 06:01:28 -0400

    First Push

---
+++ Biogen.SOATest/UserRolesService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Biogen.SOATest
+{
+    [TestClass]
+    public class UserRolesService
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(true,true);
+        }
+    }
+}
